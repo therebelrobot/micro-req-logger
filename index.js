cabe080fd44a93e58e4112fd345b573f195ac197
Ozzy Haven (Trent Oswald) <ozzyhaven@therebelrobot.com>
2017-09-11 09:57:27 -0700

    updated and remove node_modules folder

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const ksuid = require('ksuid')
 const Logger = require('@therebel/log')
 
-module.exports = function reqLog ({ serviceName, logLevel, nodeEnv }) {
+module.exports = function reqLog ({ serviceName, logLevel, nodeEnv, stats }) {
   logger = Logger({ name: `${serviceName} [${nodeEnv}]`, level: logLevel, env: nodeEnv })
   return function (handler) {
     return function (req, res) {
-      requestStart(req, res, logger)
+      requestStart(req, res, logger, stats)
       return handler(req, res).then(
         (...args) => {
-          requestFinish(req, res, logger)
+          requestFinish(req, res, logger, stats)
           return Promise.resolve(...args)
         },
         (err) => {
-          requestFinish(req, res, err, logger)
+          requestFinish(req, res, logger, stats, err)
           return Promise.reject(err) // eslint-disable-line prefer-promise-reject-errors
         }
       )
@@ -20,7 +20,7 @@ module.exports = function reqLog ({ serviceName, logLevel, nodeEnv }) {
   }
 }
 
-function requestStart (req, res, logger) {
+function requestStart (req, res, logger, stats) {
   req.reqLogger = {}
   req.reqLogger.error = null
   req.reqLogger.start = Date.now()
@@ -30,17 +30,45 @@ function requestStart (req, res, logger) {
   res.setHeader('X-Request-ID', req.reqLogger.requestId)
 }
 
-function requestFinish (req, res, error, logger) {
-  if (!logger) {
-    logger = error
-    error = null
-  }
+function requestFinish (req, res, logger, stats, error) {
   if (error && error.statusCode) res.statusCode = error.statusCode
   else if (error) res.statusCode = 500 // thrown errors are caught further up the chain in micro
   else if (!res.statusCode) res.statusCode = 200 // normal responses are attached in micro too. only custom errors, like 400s, are present right now
 
   const code = res.statusCode / 100 | 0
   const duration = Date.now() - req.reqLogger.start
+  const responseObject = {
+    method: req.method,
+    path: req.url,
+    id: req.reqLogger.requestId,
+    status: res.statusCode,
+    query: req.query
+  }
+  if (stats) {
+    const tags = [ `method:${req.method}`, `status:${res.statusCode}`, `statusGroup:${code}xx` ]
+
+    // send out stats
+    if (req.reqLogger.path) {
+      tags.push(`route:${req.reqLogger.path}`)
+      stats.histogram(`route_${req.method}_${req.reqLogger.path}`, duration)
+    }
+
+    // increment specific stats
+    stats.incr(`response_status_${code}xx`, 1, tags)
+    stats.incr(`response_status`, 1, tags)
+    stats.histogram('response_duration', duration, tags)
+
+    // warn on slow responses
+    if (duration > 1000) {
+      stats.incr('slow_response', 1, tags)
+    }
+    switch (code) {
+      case 4: // 4xx client errors
+        stats.incr('client_error', 1, tags)
+      case 5: // 5xx server errors
+        stats.incr('response_error', 1, tags)
+    }
+  }
 
   if (duration > 1000) {
     logger.warning('slow response', {
@@ -51,13 +79,15 @@ function requestFinish (req, res, error, logger) {
       route: req.reqLogger.path || undefined
     })
   }
-  if (code === 4) {
-    return logger.warning('response', { method: req.method, path: req.url, id: req.reqLogger.requestId, status: res.statusCode })
+    // increment code-specific stats and return log response
+  switch (code) {
+    case 4: // 4xx client errors
+      return logger.warning('response', responseObject)
+    case 5: // 5xx server errors
+      return logger.error('response', Object.assign({}, responseObject, { error }))
+    default: // all other responses
+      return logger.info('response', responseObject)
   }
-  if (error) {
-    return logger.error('response', { method: req.method, path: req.url, id: req.reqLogger.requestId, status: res.statusCode, query: req.query, error })
-  }
-  logger.info('response', { method: req.method, path: req.url, id: req.reqLogger.requestId, status: res.statusCode })
 }
 
 function getNormalizedPathPattern (req) {
